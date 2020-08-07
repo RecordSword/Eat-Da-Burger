f4ad5f98fba23b85ef5c82aa5d14a589fb6ad16d
Nick  Sword <nicksword@hotmail.com>
2020-08-07 21:30:07 +0800

    File structure made. Templates are not correct

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burger with Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken with Avocado', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Pulled Pork', false);
