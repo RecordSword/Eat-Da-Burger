4a79fe39e3dce03a33f9e6102fc1103da8690b2c
Nick  Sword <nicksword@hotmail.com>
2020-08-08 11:11:54 +0800

    Handlebars created and Server.js file completed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burger with Cheese', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken with Avocado', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Pulled Pork', false);
+USE burgers_db;I
+ 
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Chicken with Avocado', false),('Southern Style Pulled Pork', false), ('Beef Burger with Cheese', false);
--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-//adding __dirname + to public is what allowed me access to the images in assets/img folder
-app.use(express.static(__dirname + "/public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-//sets main.handlebars as the default layout and our view engine as handlebar
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //import routes and give the server access to them
-var routes = require("./controllers/burgersController.js");
-
+var routes = require("./controllers/burger_controller");
 app.use(routes);
 
 app.listen(PORT, function() {
