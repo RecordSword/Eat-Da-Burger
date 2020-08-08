fb457d18dfd9b35b830c5316df3be7e0321a99d9
Nick  Sword <nicksword@hotmail.com>
2020-08-08 20:28:59 +0800

    Debugging issues. Code refactored to find error with 404 status

--- config/orm.js
+++ config/orm.js
@@ -10,15 +10,21 @@ function printQuestionMarks(num) {
 
 function objToSql(ob) {
   var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    if (Object.hasOwnProperty.call(ob,key)) {
-      if(typeof value === "string" && value.indexOf (" ") >= 0) {
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations)
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      arr.push(key + "=" + value)
+      arr.push(key + "=" + value);
     }
   }
+
+  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require("express");
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
 
 var app = express();
-var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -11,14 +9,19 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
+var exphbs = require('express-handlebars')
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //import routes and give the server access to them
 var routes = require("./controllers/burger_controller");
+
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT + "/burger");
 });
